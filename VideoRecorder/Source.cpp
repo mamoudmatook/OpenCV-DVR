6368a0a0c659e5cbf6d36528d10c2084421da2d1
David Chavez <d-chavez@outlook.com>
2016-04-15 03:19:41 -0500

    Terminado guardar con modificado fps y tamaño

--- VideoRecorder/Source.cpp
+++ VideoRecorder/Source.cpp
@@ -8,6 +8,7 @@
 using namespace cv;
 using namespace std;
 
+VideoCapture gCap(1);
 VideoWriter gWriter;
 int gFps;
 int gVideoHeight;
@@ -27,13 +28,18 @@ string dateNow()
 	return str;
 }
 
-bool setNewVideo(string filePath, int fcc, int fps, Size frameSize) 
+bool setNewVideo(string filePath, string fileExtension, int fcc, int fps, Size frameSize) 
 {
-	gWriter = VideoWriter(filePath,
+	string filename;
+
+	cout << "Filename: ";
+
+	cin >> filename;
+
+	gWriter = VideoWriter(filePath + filename + dateNow() + fileExtension,
 		fcc,
 		fps,
 		frameSize);
-
 	if (!gWriter.isOpened()) {
 		cout << "Error: Opening file for write" << endl;
 		return false;
@@ -41,58 +47,63 @@ bool setNewVideo(string filePath, int fcc, int fps, Size frameSize)
 
 	return true;
 }
+
 void onChangeWidth(int, void*)
 {
+	if (gVideoWidth < 10) {
+		gVideoWidth = 10;
+	}
 	cout << "Width: " << gVideoWidth <<endl;
 	//gVideoHeight = gRatio * gVideoWidth;
 }
 
 void onChangeHeight(int, void*)
 {
+	if (gVideoHeight < 10) {
+		gVideoHeight = 10;
+	}
 	cout << "Height: " << gVideoHeight << endl;
 	//gVideoWidth = gRatio * gVideoHeight;
 }
 
 void onChangeFps(int, void*)
 {
+	if (gFps < 1) {
+		gFps = 1;
+	}
+	//gCap.set(CV_CAP_PROP_FPS, gFps);
 	cout << "FPS: " << gFps << endl;
-}
-void onClickRec(int, void*)
-{
-	gIsRecording = true;
-	cout << "Recording" << endl;
-}
-void onClickPause(int, void*)
-{
-	gIsRecording = false;
-	cout << "Paused" << endl;
-}
-void onClickSave(int, void*)
-{
-
 }
 
 int main()
 {
-	VideoCapture cap(0);
+	//VideoCapture cap(1);
 	Mat frame;
 	Mat resized;
 	IplImage* image;
-	gFps = 10;
+	string filepath = "C:\\users\\david\\";
+	string fileExtension = ".avi";
+	int fcc = CV_FOURCC('D', 'X', '5', '0');
 	gIsRecording = false;
+	bool isFirstRun = true;
 
 	try {
 		
 		std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 
-		if (!cap.isOpened()) {
+		if (!gCap.isOpened()) {
 			cout << "Error: Video capture" << endl;
 			system("pause");
 			return -1;
 		}
 
-		double frameWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH);
-		double frameHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+		double frameWidth = gCap.get(CV_CAP_PROP_FRAME_WIDTH);
+		double frameHeight = gCap.get(CV_CAP_PROP_FRAME_HEIGHT);
+		gFps = gCap.get(CV_CAP_PROP_FPS);
+		if (gFps == 0) 
+		{
+			gFps = 20;
+		}
 		//Size frameSize((int)frameWidth, (int)frameHeight);
 
 		gRatio = frameHeight / frameWidth;
@@ -106,38 +117,72 @@ int main()
 		createTrackbar("Width", "Video Settings", &gVideoWidth, gVideoWidth, onChangeWidth);
 		createTrackbar("Height", "Video Settings", &gVideoHeight, gVideoHeight, onChangeWidth);
 		createTrackbar("Fps", "Video Settings", &gFps, 60, onChangeFps);
-		//createButton("Rec", onClickRec);
-		//createButton("Pause", onClickPause);
-		//createButton("Save", onClickSave);
 		image = cvCreateImage(cvSize(frameWidth, 10), IPL_DEPTH_8U, 3);
 		Mat guiFrame(image);
 
 		while (1) {
 
-			if (!cap.read(frame))
+			//filename = "VideoCapture" + dateNow();
+
+			if (!gCap.read(frame))
 			{
 				cout << "Error: Reading frame from camera feed" << endl;
 				break;
 			}
 
 			resize(frame, resized, Size(gVideoWidth, gVideoHeight));
-			imshow(windowName, resized);
-			
+
+			if (gIsRecording) 
+			{
+				gWriter.write(resized);
+				putText(resized, "REC", Point(5, 20), 1, 1, Scalar(0, 0, 255), 2, 7);
+			}
+
 			imshow("Video Settings", guiFrame);
 
+			imshow(windowName, resized);
+			
 			switch (waitKey(10))
 			{
-			case 27:
-				return 0;
+				//n ew
+				case 110:
+					setNewVideo(filepath,
+						fileExtension,
+						fcc,
+						gFps,
+						Size(gVideoWidth, gVideoHeight));
+				break;
+				//s ave
+				case 115:
+
+				break;
+				//r ec
+				case 114:
+					gIsRecording = !gIsRecording;
+					if (isFirstRun) 
+					{
+						isFirstRun = false;
+						if (!setNewVideo(filepath,
+							fileExtension,
+							fcc,
+							gFps,
+							Size(gVideoWidth, gVideoHeight))) {
+							throw new exception();
+						}
+					}
+				break;
+				//esc ape
+				case 27:
+					return 0;
 				break;
 			}
 		}
 
-		cap.release();
+		gCap.release();
 	}
 	catch (Exception ex) {
-		if (cap.isOpened()) {
-			cap.release();
+		if (gCap.isOpened()) {
+			gCap.release();
 		}
 	}
 
